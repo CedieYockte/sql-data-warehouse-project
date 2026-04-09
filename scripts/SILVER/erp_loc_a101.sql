9a9fd5c9f1899551cecc500060ed3251a7d03555
CedieYockte <71365116+CedieYockte@users.noreply.github.com>
2026-04-10 02:04:58 +0800

    Insert script to silver_erp_loc_a101
    Insert data into SILVER.erp_loc_a101 with country name adjustments.

---
+++ scripts/SILVER/erp_loc_a101.sql
@@ -0,0 +1,13 @@
+INSERT INTO SILVER.erp_loc_a101 (
+	cid,
+	cntry
+)
+SELECT
+	REPLACE(cid, '-', '') AS cid, -- removes the hypen in the ID
+	CASE
+		WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+		WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		WHEN TRIM(cntry) = '' OR TRIM(cntry) IS NULL THEN 'n/a'
+		ELSE TRIM(cntry) 
+	END	AS cntry -- Properly setup the country
+FROM BRONZE.erp_loc_a101
